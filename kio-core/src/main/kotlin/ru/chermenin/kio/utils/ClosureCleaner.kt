02c60dfcf6b71147e1b97e1bc40d9df45397e2cd
Alex Chermenin <alex@chermenin.ru>
2025-11-01 11:16:50 +0500

    Refactored POMs and versions

--- kio-core/src/main/kotlin/ru/chermenin/kio/utils/ClosureCleaner.kt
+++ kio-core/src/main/kotlin/ru/chermenin/kio/utils/ClosureCleaner.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Alex Chermenin
+ * Copyright 2020 Alex Chermenin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
